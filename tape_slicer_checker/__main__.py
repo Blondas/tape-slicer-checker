b39d4f93444da2c3a1cf842201f2498524206287
Krzysiek Nowakowski <knowakowski@pm.me>
2025-02-11 15:19:18 +0100

    feat(main.py): add command-line argument parsing for input and output directories and tape name to enhance usability refactor(logging_setup.py): modify logging setup to accept output directory instead of logging config to simplify logging configuration chore(config.py): remove unused logging and checker configurations from TapeSlicerCheckerConfig to streamline the configuration structure feat(delete_path.py): implement delete_path utility function to handle directory deletion safely and log actions docs(tape_slicer_checker_config_SAMPLE.yaml): update sample config file to remove obsolete logging and checker configurations for clarity

--- tape_slicer_checker/__main__.py
+++ tape_slicer_checker/__main__.py
@@ -1,47 +1,56 @@
 import logging
 from pathlib import Path
+import argparse
 
-from tape_slicer_checker.config.tape_slicer_checker_config import TapeSlicerCheckerConfig, load_config
 from tape_slicer_checker.logging import logging_setup
+from tape_slicer_checker.config.tape_slicer_checker_config import TapeSlicerCheckerConfig, load_config
 from tape_slicer_checker.services.ars_admin_retrieve_executor_impl import ArsAdminRetrieveExecutorImpl
 from tape_slicer_checker.services.checksum_verifier_impl import ChecksumVerifierImpl
 from tape_slicer_checker.services.cmd_parameters import CmdParameters
 from tape_slicer_checker.services.cmd_params_lookup_impl import CmdParamsLookupImpl
 from tape_slicer_checker.services.combined_table_lookup_impl import CombinedTableLookupImpl
 from tape_slicer_checker.db2.db2_connection import DB2Connection
+from tape_slicer_checker.utils.delete_path import delete_path
 
 logger = logging.getLogger(__name__)
 
-if __name__ == '__main__':
-    config: TapeSlicerCheckerConfig = load_config('./tape_slicer_checker/resources/tape_slicer_checker_config.yaml')
+def parse_args():
+    parser = argparse.ArgumentParser(description='Tape Slicer Checker')
+    parser.add_argument('--input-dir', type=Path, required=True, help='Input directory')
+    parser.add_argument('--tape-name', type=str, required=True, help='Tape name')
+    parser.add_argument('--output-dir', type=Path, required=True, help='Output directory')
+    return parser.parse_args()
 
-    logging_setup.setup_logging(config.logging_config)
+if __name__ == '__main__':
+    args = parse_args()
+
+    config: TapeSlicerCheckerConfig = load_config('./tape_slicer_checker/resources/tape_slicer_checker_config.yaml')
+    logging_setup.setup_logging(args.output_dir)
     logger.info("tape_slicer_checker starting...")
-    
+
     db2_connection = DB2Connection(config.db_config)
-    combined_table_lookup = CombinedTableLookupImpl(db2_connection, config.checker_config.source_dir.name)
+    combined_table_lookup = CombinedTableLookupImpl(db2_connection, args.tape_name)
     cmd_params_lookup = CmdParamsLookupImpl(combined_table_lookup)
     ars_admin_retrieve_executor = ArsAdminRetrieveExecutorImpl(config.ars_admin_retrieve_config)
     checksum_verifier = ChecksumVerifierImpl()
-    
-    path_objects: list[tuple[Path, CmdParameters]] = cmd_params_lookup.process_directory(config.checker_config.source_dir)
+
+    path_objects: list[tuple[Path, CmdParameters]] = cmd_params_lookup.process_directory(args.source_dir)
     objects: list[CmdParameters] = [elem[1] for elem in path_objects]
-    logger.info(f"Arsadmin parameters fetched from db for {len(path_objects)} objects from {config.checker_config.source_dir}")
-    
+    logger.info(f"Arsadmin parameters fetched from db for {len(path_objects)} objects from {args.source_dir}")
+
     ars_admin_retrieve_executor.execute_commands(objects)
     logger.info("Arsadmin retrieve command executed.")
-    
+
     file_pairs: list[tuple[Path, Path]] = [
-        (elem[0], config.ars_admin_retrieve_config.output_dir / elem[1].ag_name / elem[1].file_name) 
+        (elem[0], args.output_dir / elem[1].ag_name / elem[1].file_name) 
         for elem in path_objects
     ]
-    
+
     failed_verification: list[tuple[Path, Path]] = checksum_verifier.verify(file_pairs)
     logger.info("Files verification finished")
-    
+
     if failed_verification:
         logger.error(f"Number of failed pairs: {len(failed_verification)}")
     else:
-        logger.info("All pairs are correct.")
-    
-    
+        delete_path(args.output_dir)
+        logger.info("All pairs are correct. Output directory deleted")
